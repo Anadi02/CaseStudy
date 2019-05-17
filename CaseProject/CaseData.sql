e7bea294ba1bbe85ff86d40d657d97c0376d1423
Anadi Mishra <anadi.mishra@dxc.com>
2019-05-17 12:48:51 +0530

    tables created successful.

--- CaseProject/CaseData.sql
+++ CaseProject/CaseData.sql
@@ -1,1 +1,24 @@
 ﻿CREATE database CasestudyDB
+use CasestudyDB
+Create table Workout_Category(
+category_id int identity primary key,
+category_name varchar (64)
+)
+Create table Workout_Collection(
+Workout_title varchar(128),
+WorkoutNote  varchar(256),
+Calories_Burn_Per_min float,
+category_id int foreign key references Workout_Category(category_id),
+workout_id int primary key
+
+)
+Create table Workout_Active(
+Workout_id int foreign key references Workout_Collection(workout_id),
+start_time time,
+starts_date date,
+end_time time,
+end_date date,
+comment varchar(64),
+statuss bit
+)
+
